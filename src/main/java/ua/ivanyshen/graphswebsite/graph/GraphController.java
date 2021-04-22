f4b1ffa1930572765b942a47df92dc6b2534e569
MaxIvanyshen <maxivanyshen@gmail.com>
2021-04-22 15:19:46 +0300

    back-up

--- src/main/java/ua/ivanyshen/graphswebsite/graph/GraphController.java
+++ src/main/java/ua/ivanyshen/graphswebsite/graph/GraphController.java
@@ -17,18 +17,23 @@ public class GraphController {
     private String websiteName = "Viz4Charts";
 
     /**
-     * In line model.addAttribute("title", websiteName); we create a local variable for thymeleaf
+     * In line model.addAttribute("title", websiteName); we create a local variable for Thymeleaf
      * called "title" with value of "Viz4Charts"
+     *
+     * In line model.addAttribute("chart", new Graph()); or
+     * model.addAttribute("chart", graph);
+     * we create a local variable for Thymeleaf
+     * called "chart" with our new chart or already created one
      */
 
-    //Creates '/' route of website
+    //Creates '/' route of website with home page
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("title", websiteName);
         return "index";
     }
 
-    //Creates '/create-charts' route of website
+    //Creates '/create-charts' route of website with charts creator
     @GetMapping("/create-charts")
     public String createCharts(Model model) {
         model.addAttribute("title", websiteName);
@@ -37,6 +42,8 @@ public class GraphController {
         return "enter-rows";
     }
 
+    //Operates post request from 'create-charts' route: creates as
+    // many params and values for them as we want
     @PostMapping("/create-charts")
     public String chooseParamsAndValues(Model model, @ModelAttribute Graph graph) {
         model.addAttribute("title", websiteName);
@@ -44,29 +51,30 @@ public class GraphController {
         return "chooseParamsAndValues";
     }
 
+    //Operates post request from 'create-charts' route: creates
+    // chart with params and values we entered in the previous route
     @PostMapping("/editor")
     public String chartEditor(Model model, @ModelAttribute Graph graph) {
         model.addAttribute("title", websiteName);
         model.addAttribute("chart", graph);
-        System.out.println(Arrays.toString(graph.getParams()));
         return "chartEditor";
     }
 
-    //Creates '/contact' route of website
+    //Creates '/contact' route of website with contact page
     @GetMapping("/contact")
     public String contact(Model model) {
         model.addAttribute("title", websiteName);
         return "contact";
     }
 
-    //Creates '/premium' route of website
+    //Creates '/premium' route of website with page for premium payment
     @GetMapping("/premium")
     public String premium(Model model) {
         model.addAttribute("title", websiteName);
         return "premium";
     }
 
-    //Creates '/sign-up' route of website
+    //Creates '/sign-up' route of website with page for craeting an account
     @GetMapping("/sign-up")
     public String signUp(Model model) {
         model.addAttribute("title", websiteName);
